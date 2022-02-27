d5d1bd5a28dc3c05c21ed19a4eee0c5a1c491482
seleuchel <seleuchel@gmail.com>
2022-02-28 00:35:56 +0900

    debug_little

--- cicat/example/CCATrun.bat
+++ cicat/example/CCATrun.bat
@@ -1,1 +1,1 @@
-python ..\generator\scenGEN.py -r -i INFRASTRUCTURE.ap1000.xlsx -s SCENARIOS.ap1000.xlsx -o RESULTS.ap1000.xlsx
+python ..\generator\scenGEN.py -r -i ./this/0227_edit_INFRASTRUCTURE.ap1000.xlsx -s ./this/0227_edit_SCENARIOS.ap1000.xlsx -o ./this/0227_edit_RESULTS.ap1000.xlsx
